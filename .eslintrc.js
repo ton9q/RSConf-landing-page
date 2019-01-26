4d059f9dcd1117795b560540d7fa9e7f93a670d7
saniok017 <saniok017@gmail.com>
2019-01-26 18:09:33 +0300

    docs: - add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+        "browser": true,
+        "node": true,
+        "jest": true,
+        "jsx": true
+    },
+    "plugins": [
+        "react",
+        "jsx-ally",
+        "import"
+    ]
+};
