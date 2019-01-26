6198067dfa6ddbcfee0016763305f2e3f095bee1
ton9q <kuchma_anton@mail.ru>
2019-01-27 00:25:23 +0300

    refactor: add components for good view code

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,128 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import { Media } from 'react-bootstrap'
+
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import personDescription from '../components/personDescription';
-import { Media } from 'react-bootstrap'
+import PersonDescription from '../components/personDescription'
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <Media>
-        <img
-          width={64}
-          height={64}
-          className="mr-3"
-          src="holder.js/64x64"
-          alt="Generic placeholder"
-        />
-        <Media.Body>
-        <h5>Media Heading</h5>
-        <p>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-            ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-            tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
-            Donec lacinia congue felis in faucibus.
-        </p>
-        </Media.Body>
-    </Media>
+    <PersonDescription />
     <hr />
-    <Media>
-      <Media.Body>
-        <h5>Media Heading</h5>
-        <p>
-          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-          ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-          tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
-          Donec lacinia congue felis in faucibus.
-        </p>
-      </Media.Body>
-      <img
-        width={64}
-        height={64}
-        className="ml-3"
-        src="holder.js/64x64"
-        alt="Generic placeholder"
-      />
-    </Media>
-    <hr />
-    <Media>
-        <img
-          width={64}
-          height={64}
-          className="mr-3"
-          src="holder.js/64x64"
-          alt="Generic placeholder"
-        />
-        <Media.Body>
-        <h5>Media Heading</h5>
-        <p>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-            ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-            tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
-            Donec lacinia congue felis in faucibus.
-        </p>
-        </Media.Body>
-    </Media>
-    <hr />
-    <Media>
-      <Media.Body>
-        <h5>Media Heading</h5>
-        <p>
-          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-          ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-          tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
-          Donec lacinia congue felis in faucibus.
-        </p>
-      </Media.Body>
-      <img
-        width={64}
-        height={64}
-        className="ml-3"
-        src="holder.js/64x64"
-        alt="Generic placeholder"
-      />
-    </Media>
-    <hr />
-    <Media>
-        <img
-          width={64}
-          height={64}
-          className="mr-3"
-          src="holder.js/64x64"
-          alt="Generic placeholder"
-        />
-        <Media.Body>
-        <h5>Media Heading</h5>
-        <p>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-            ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-            tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
-            Donec lacinia congue felis in faucibus.
-        </p>
-        </Media.Body>
-    </Media>
-    <hr />
-    <Media>
-      <Media.Body>
-        <h5>Media Heading</h5>
-        <p>
-          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-          ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-          tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
-          Donec lacinia congue felis in faucibus.
-        </p>
-      </Media.Body>
-      <img
-        width={64}
-        height={64}
-        className="ml-3"
-        src="holder.js/64x64"
-        alt="Generic placeholder"
-      />
-    </Media>
-    <personDescription />
+    <PersonDescription />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
