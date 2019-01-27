f56c07c700954f2aa18b9b42e6ffaba2bc2b3457
saniok017 <saniok017@gmail.com>
2019-01-27 16:55:18 +0300

    feat: add person render module

--- src/components/producer/producer.js
+++ src/components/producer/producer.js
@@ -1,14 +1,32 @@
-<h1>Фамилия Имя</h1>
-<Figure>
-  <Figure.Image
-    width={400}
-    height={500}
-    alt={person}
-    src="holder.js/171x180"
-  />
-  {/* <Figure.Caption>
-  Фамилия Имя Отчество
-  </Figure.Caption> */}
-</Figure>
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { Figure } from 'react-bootstrap'
 
-// собирает все остальные компоненты
+const Person = ({ person, src }) => (
+  <Fragment>
+    <Figure>
+      <Figure.Image
+        width={400}
+        height={500}
+        alt={person}
+        src={src}
+      />
+      <Figure.Caption>
+      {person}
+      </Figure.Caption>
+    </Figure>
+    {/* here comes the other parts */}
+  </Fragment>
+)
+
+Person.defaultProps = {
+  person: 'Albert Einstein',
+  src: 'https://duckduckgo.com/i/45956a7f.jpg',
+}
+
+Person.propTypes = {
+  person: PropTypes.string,
+  src: PropTypes.string,
+}
+
+export default Person
