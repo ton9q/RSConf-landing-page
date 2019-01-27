925a0e7aea6268975ce812a8ae89f4854a8fed06
saniok017 <saniok017@gmail.com>
2019-01-27 23:49:45 +0300

    refactor: remove state to enother module

--- src/components/personListHandler.js
+++ src/components/personListHandler.js
@@ -1,30 +1,13 @@
 import React, { Component } from 'react'
 import { CardGroup } from 'react-bootstrap'
-
-import producers from '../../data/producers-rus.json'
+import producerState from "./producerState";
 
 import Person from './person'
-
-import pic0 from '../images/producers/0/0.jpg'
-import pic1 from '../images/producers/1/0.jpg'
-import pic2 from '../images/producers/2/0.jpg'
-import pic3 from '../images/producers/3/0.jpg'
-import pic4 from '../images/producers/4/0.jpg'
-import pic5 from '../images/producers/5/0.jpg'
-// import GlobalState from 'globalstate'
-
-import { ProducerState, setState, getState } from './producerState';
 import { object } from 'prop-types';
 
-const producerState = new ProducerState();
-setState(producerState);
-
 export default class PersonListHandler extends Component {
-  state = {
-    producers,
-    pictures: [pic0, pic1, pic2, pic3, pic4, pic5],
-  }
-  
+  state = producerState
+
   handleClick = (e) => {
     if (e.target.tagName === 'BUTTON') {
       localStorage.setItem('producerName', `${e.currentTarget.className}`)
@@ -41,7 +24,7 @@ export default class PersonListHandler extends Component {
           <div className={person.person} key={`${index} - ${person.person}`} onClick={this.handleClick}>
             <Person
               person={person.person}
-              linkImage={pictures[index]}
+              linkImage={pictures[index][0]}
               linkButton="/person"
               buttonName="more"
             />
--- src/components/producerState.js
+++ src/components/producerState.js
@@ -1,21 +1,61 @@
-export class ProducerState {
-  constructor() {
-    this.producerName = '';
-  }
+import pic0 from '../images/producers/0/0.jpg'
+import pic1 from '../images/producers/0/1.jpg'
+import pic2 from '../images/producers/0/2.jpg'
+import pic3 from '../images/producers/1/0.jpg'
+import pic4 from '../images/producers/1/1.jpg'
+import pic5 from '../images/producers/1/2.jpg'
+import pic6 from '../images/producers/1/3.jpg'
+import pic7 from '../images/producers/1/4.jpg'
+import pic8 from '../images/producers/2/0.jpg'
+import pic9 from '../images/producers/2/1.jpg'
+import pic10 from '../images/producers/3/0.jpg'
+import pic11 from '../images/producers/3/1.jpg'
+import pic12 from '../images/producers/3/2.jpg'
+import pic13 from '../images/producers/3/3.jpg'
+import pic14 from '../images/producers/3/4.jpg'
+import pic15 from '../images/producers/4/0.jpg'
+import pic16 from '../images/producers/4/1.jpg'
+import pic17 from '../images/producers/4/2.jpg'
+import pic18 from '../images/producers/4/3.jpg'
+import pic19 from '../images/producers/5/0.jpg'
+import pic20 from '../images/producers/5/1.jpg'
+import pic21 from '../images/producers/5/2.jpg'
+import pic22 from '../images/producers/5/3.jpg'
+import pic23 from '../images/producers/5/4.jpg'
+import pic24 from '../images/producers/5/5.jpg'
+import pic25 from '../images/producers/5/6.jpg'
 
-  setProducerName(name = '') {
-    this.producerName = name;
-  }
+import producers from '../../data/producers-rus.json'
+
+const pictures = [
+  [pic0,
+  pic1,
+  pic2],
+  [pic3,
+  pic4,
+  pic5,
+  pic6,
+  pic7],
+  [pic8,
+  pic9],
+  [pic10,
+  pic11,
+  pic12,
+  pic13,
+  pic14],
+  [pic15,
+  pic16,
+  pic17,
+  pic18],
+  [pic19,
+  pic20,
+  pic21,
+  pic22,
+  pic23,
+  pic24,
+  pic25]
+]
+export default {
+  producers,
+  pictures
 }
-
-let producerState = null;
-
-export const setState = (state) => {
-  console.log('state', state)
-  producerState = state;
-};
-
-export const getState = () => {
-  console.log('getStaet', producerState);
-  producerState
-};
