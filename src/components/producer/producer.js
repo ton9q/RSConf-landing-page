050335cee1fe707f040113218ed6bc1b1fdc65e2
ton9q <kuchma_anton@mail.ru>
2019-01-27 15:08:07 +0300

    refactor(dirty): posted to components code

---
+++ src/components/producer/producer.js
@@ -0,0 +1,14 @@
+<h1>Фамилия Имя</h1>
+<Figure>
+  <Figure.Image
+    width={400}
+    height={500}
+    alt={person}
+    src="holder.js/171x180"
+  />
+  {/* <Figure.Caption>
+  Фамилия Имя Отчество
+  </Figure.Caption> */}
+</Figure>
+
+// собирает все остальные компоненты
