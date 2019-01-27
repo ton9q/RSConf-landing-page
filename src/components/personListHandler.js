2d4e6e20ec47c7bf5a3fdf00425f7dbb56f9f55c
saniok017 <saniok017@gmail.com>
2019-01-27 17:34:27 +0300

    feat: add handleClick feature

--- src/components/personListHandler.js
+++ src/components/personListHandler.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { CardGroup, Alert } from 'react-bootstrap'
+import { CardGroup } from 'react-bootstrap'
 
 import producers from '../../data/producers-rus.json'
 
@@ -11,6 +11,7 @@ import pic2 from '../images/producers/2/0.jpg'
 import pic3 from '../images/producers/3/0.jpg'
 import pic4 from '../images/producers/4/0.jpg'
 import pic5 from '../images/producers/5/0.jpg'
+// import GlobalState from 'globalstate'
 
 export default class PersonListHandler extends Component {
   state = {
@@ -19,7 +20,7 @@ export default class PersonListHandler extends Component {
   }
 
   handleClick = (e) => {
-    console.dir(e.key);
+    globalState.setState(e.currentTarget.className)
   }
 
   render() {
@@ -28,7 +29,7 @@ export default class PersonListHandler extends Component {
     return (
       <CardGroup>
         {producers.map((person, index) => (
-          <div key={`${index} - ${person}`} onClick={this.handleClick}>
+          <div className={person.person} key={`${index} - ${person.person}`} onClick={this.handleClick}>
             <Person
               person={person.person}
               linkImage={pictures[index]}
