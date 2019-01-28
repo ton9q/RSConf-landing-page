e27f65d41b66d18c65f26d95b31a20ff9c508632
ton9q <kuchma_anton@mail.ru>
2019-01-28 16:22:39 +0300

    refector: change presentation team page, add dependencies for vocabulary

--- src/pages/team.js
+++ src/pages/team.js
@@ -4,35 +4,21 @@ import { CardGroup } from 'react-bootstrap';
 import Layout from '../components/layout';
 import Person from '../components/person';
 
+import team from '../../data/team.json';
+
 const Team = () => (
   <Layout>
     <h1>&ldquo;Group 23&rdquo; Team</h1>
 
     <CardGroup>
-      <Person
-        nickname="Saniok017"
-        person="Aliaksandr Naumenka"
-        linkImage="https://avatars3.githubusercontent.com/u/39351424?s=400&v=4"
-        linkButton="https://github.com/saniok017"
-      />
-      <Person
-        nickname="sergey496"
-        person="Siarhei Zylevich"
-        linkImage="https://avatars0.githubusercontent.com/u/21264092?s=400&v=4"
-        linkButton="https://github.com/sergey496"
-      />
-      <Person
-        nickname="ton9q"
-        person="Anton Kuchma"
-        linkImage="https://avatars3.githubusercontent.com/u/26420726?s=400&v=4"
-        linkButton="https://github.com/ton9q"
-      />
-      <Person
-        nickname="alexeykuril0"
-        person="Alexey Kurilo"
-        linkImage="https://avatars0.githubusercontent.com/u/43179292?s=400&v=4"
-        linkButton="https://github.com/alexeykuril0"
-      />
+      {team.map(teammate => (
+        <Person
+          nickname={teammate.nickaname}
+          person={teammate.person}
+          linkImage={teammate.linkImage}
+          linkButton={teammate.linkButton}
+        />
+      ))}
     </CardGroup>
   </Layout>
 );
