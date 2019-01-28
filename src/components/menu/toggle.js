4f1aba161bf757ac58120ec36798c5fba0a32bfd
ton9q <kuchma_anton@mail.ru>
2019-01-28 19:06:58 +0300

    refactor(dirty): change toggle component

--- src/components/menu/toggle.js
+++ src/components/menu/toggle.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import { ButtonToolbar, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
-import { Trans } from 'react-i18next';
+/* eslint-disable */
+import React, { Component } from 'react'
+import { ButtonToolbar, ToggleButtonGroup, ToggleButton } from 'react-bootstrap'
+import i18n from 'i18next'
+import { Trans } from 'react-i18next'
 
-const Toggle = () => (
-  <ButtonToolbar>
-    <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-      <ToggleButton value={1}><Trans>Rus</Trans></ToggleButton>
-      <ToggleButton value={2}><Trans>Eng</Trans></ToggleButton>
-    </ToggleButtonGroup>
-  </ButtonToolbar>
-);
+import producerState from '../../utils/producerState'
 
-export default Toggle;
+import producersRus from '../../../data/producers-rus.json'
+import producersEng from '../../../data/producers-eng.json'
+
+export default class Toggle extends Component {
+  changeLang(e, lang) {
+    // e.preventDefault();
+    // console.log(lang);
+
+    i18n.changeLanguage(lang);
+
+    producerState.lang = lang;
+    if (lang === 'ru') {
+      producerState.producers = producersRus;
+    } else if (lang === 'en') {
+      producerState.producers = producersEng;
+    }
+    // console.log(producerState)
+  }
+
+  render() {
+    return (
+      <ButtonToolbar>
+        <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+          <ToggleButton value={1} onClick={(e) => this.changeLang(e, 'ru')}>
+            <Trans>Rus</Trans>
+          </ToggleButton>
+          <ToggleButton value={2} onClick={(e) => this.changeLang(e, 'en')}>
+            <Trans>Eng</Trans>
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </ButtonToolbar>
+    )
+  }
+}
